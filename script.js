352e32ee80c773741ecca5d97a7152d7687213ab
Wendy Quarteer <wendyquarteer@outlook.com>
2022-07-27 11:39:13 +0200

    dog goes up- and down on key spacebar

--- script.js
+++ script.js
@@ -14,9 +14,29 @@ function moveBeach(){
     const beach = document.getElementById("beach");
     pos--;
     beach.style.backgroundPositionX = pos + "px";
-    setTimeout(moveBeach, -160)
+    setTimeout(moveBeach, -160);
 }
 moveBeach()
+
+
+
+function dogUp(e) {
+    if(e.keyCode === 32){
+            const dog = document.getElementById("dog");
+            dog.style.bottom = "455px";
+            dog.style.transition = "bottom 4s ease-in-out";
+    }
+}
+
+function dogDown(e) {
+    if(e.keyCode === 32){
+            const dog = document.getElementById("dog");
+            dog.style.bottom = "35px";
+            dog.style.transition = "bottom 1s ease-in-out";
+    }
+}
+document.addEventListener('keydown', dogUp);
+document.addEventListener('keyup', dogDown);
 
 
 
